15545e1d8f2ab68afd1d32de62688442cc267289
João Vitor Mateus Silva <joaovitor2207@gmail.com>
2019-06-23 16:13:15 -0300

    Mais mudanças na tela

--- br.ufsc.ine.sistemas.5605/src/src/br/ufsc/ine5605/telas/TelaBatalha.java
+++ br.ufsc.ine.sistemas.5605/src/src/br/ufsc/ine5605/telas/TelaBatalha.java
@@ -7,14 +7,18 @@ package src.br.ufsc.ine5605.telas;
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.Scanner;
 import src.br.ufsc.ine5605.controllers.BatalhaController;
 import java.util.Date;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 
 /**
  *
@@ -24,31 +28,63 @@ public class TelaBatalha extends JFrame {
 
     private JButton selecionarLutadoresBtn;
     private JButton batalharBtn;
-    private BatalhaController batalhaControll;
+    private JButton apagarBatalhaBtn;
+    private JButton cancelarBtn;
+
+    private JLabel aliadoLabel;
+    private JLabel adversarioLabel;
+
+    private JTextField aliadoField;
+    private JTextField adversarioField;
+
+    private JTable tabela;
+
     private Scanner teclado = new Scanner(System.in);
-    private JTable fundo;
 
     public TelaBatalha() {
         super("Batalha");
-        setLayout(new FlowLayout());
+        JPanel panel = new JPanel(new GridBagLayout());
+        this.getContentPane().setLayout(new GridBagLayout());
+        this.getContentPane().add(panel);
+        GridBagConstraints gbc = new GridBagConstraints();
+
         String[] columnNames = {"Titulo", "Pokemon Vencedor", "Pokemon Derrotado"};
         Object[][] data = {
             {"Batalha 01", "Pikacu", "bulbassalto"},
             {"Batalha 02", "Crackzard", "lsdevee"}
         };
-        fundo = new JTable(data, columnNames);
-        fundo.setPreferredScrollableViewportSize(new Dimension(500, 50));
-        fundo.setFillsViewportHeight(true);
-        
-        JScrollPane scrollPane = new JScrollPane(fundo);
+        tabela = new JTable(data, columnNames);
+        tabela.setPreferredScrollableViewportSize(new Dimension(500, 50));
+        tabela.setFillsViewportHeight(true);
+
+        JScrollPane scrollPane = new JScrollPane(tabela);
         add(scrollPane);
-                
-                
+
         selecionarLutadoresBtn = new JButton();
         selecionarLutadoresBtn.setText("Selecione os Lutadores");
         selecionarLutadoresBtn.setToolTipText("Selecione os Pokemons para a batalha");
         add(selecionarLutadoresBtn);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //------------------------------------------------------------------------------------------------//
+        apagarBatalhaBtn = new JButton();
+        apagarBatalhaBtn.setText("Apagar Batalha");
+        apagarBatalhaBtn.setToolTipText("Apague uma Batalha Selecionada");
+        add(apagarBatalhaBtn);
+        //-----------------------------------------------------------------------------------------------//
+        cancelarBtn = new JButton();
+        cancelarBtn.setText("Cancelar");
+        cancelarBtn.setToolTipText("Cancelar ação em andamento");
+        add(cancelarBtn);
+        //-----------------------------------------------------------------------------------------------//
+        aliadoLabel = new JLabel();
+        aliadoLabel.setText("Aliado:");
+        aliadoLabel.setToolTipText("Selecione o seu Pokemon para a batalha");
+        add(aliadoLabel);
+        //------------------------------------------------------------------------------------------------//
+        adversarioLabel = new JLabel();
+        adversarioLabel.setText("Aliado:");
+        adversarioLabel.setToolTipText("Selecione o Pokemon adversário para a batalha, ou clique em aleatorio");
+        add(adversarioLabel);
+
     }
 
     public void selecionarLutadores() {
@@ -58,7 +94,7 @@ public class TelaBatalha extends JFrame {
         System.out.println("Insira o nome do Pokemon selvagem: ");
         String nomeSelvagem = s.nextLine();
 
-        batalhaControll.batalhar(nomeMyPokemon, nomeSelvagem);
+        BatalhaController.getInstancia().batalhar(nomeMyPokemon, nomeSelvagem);
     }
 }
 // public void listarTarefas() {
